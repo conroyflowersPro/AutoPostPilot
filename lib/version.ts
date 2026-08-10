30613bd67fdc768d0a0dd0935bf2200b879e5474
conroyflowersPro <conroyf12@gmail.com>
2026-08-09 19:34:35 -0700

    feat: universal topic resolver for all slots (screenshot + strategic)

--- lib/version.ts
+++ lib/version.ts
@@ -1,4 +1,4 @@
 /** Single source of truth for AutoPostPilot product version (UI badges + package). */
-export const APP_VERSION = "7.3.11";
+export const APP_VERSION = "7.3.12";
 export const APP_VERSION_LABEL = `v${APP_VERSION}`;
-export const BUILD_STAMP = "2026-08-09-v7.3.11-draft-content-column";
+export const BUILD_STAMP = "2026-08-09-v7.3.12-universal-topic-resolver";
